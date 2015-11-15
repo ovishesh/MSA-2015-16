5c62e7030a4401df1972cd1a91d7007645333e01
Alyssa Ong <aong844@aucklanduni.ac.nz>
2015-11-16 11:52:27 +1300

    First commit server side only

---
+++ MSAUniApp/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MSAUniApp.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
