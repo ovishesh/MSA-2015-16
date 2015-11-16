d6fd870c72013f0d544792affbd2caf9f13c0dba
Zenon Colaco <zncolaco@gmail.com>
2015-11-17 11:31:57 +1300

    changed studentmodule to jquery

--- Front End Full/scripts/studentmodule.js
+++ Front End Full/scripts/studentmodule.js
@@ -2,99 +2,68 @@
 
     return {
         getStudents: function (callback) {
-
-            var xhttp = new XMLHttpRequest();
-            
-            //This gets triggered when the state of the xhttp object changes
-            xhttp.onreadystatechange = function () {
-                // 4 - repsonse is ready, 200 success code
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    loadedStudents();
+             $.ajax({ 
+                type: "GET",
+                dataType: "json",
+                url: "http://msauniversity.azurewebsites.net/api/Students",
+                success: function(data){        
+                    console.log(data);
+                    callback(data);
                 }
-            }
-            
-            // Build up our request and send it - true for async
-            xhttp.open("GET", "http://msauniversity.azurewebsites.net/api/Students", true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-            xhttp.send(null);
-            
-            // Parse and send the studentlist data back to index.js
-            function loadedStudents() {
-                var studentsList = JSON.parse(xhttp.responseText);
-                callback(studentsList);
-                return studentsList;
-            }
+             });
         },
 
         getStudentById: function (id, callback){
-
-            var xhttp = new XMLHttpRequest();
-
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    loadedStudent();
+            
+            $.ajax({ 
+                type: "GET",
+                dataType: "json",
+                url: "http://msauniversity.azurewebsites.net/api/Students/" + id,
+                success: function(data){        
+                    console.log(data);
+                    callback(data);
                 }
-            }
+             });
 
-            xhttp.open("GET", "http://msauniversity.azurewebsites.net/api/Students/" + id, true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-
-            xhttp.send();
-
-            function loadedStudent() {
-                var student = JSON.parse(xhttp.responseText);
-                callback(student);
-                return student;
-            }
         },
 
         addStudent: function (student, callback) {
-            
-            var xhttp = new XMLHttpRequest();
-
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 201) {
+             
+             $.ajax({
+                url : "http://msauniversity.azurewebsites.net/api/Students/",
+                type: "POST",
+                data : student,
+                success: function(data, textStatus, jqXHR)
+                {
                     callback();
                 }
-            }
-
-            xhttp.open("POST", "http://msauniversity.azurewebsites.net/api/Students", true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-
-            xhttp.send(JSON.stringify(student));
+             });
 
         },
 
         updateStudent: function (studentid, student, callback){
-
-            var xhttp = new XMLHttpRequest();
-
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 204) {
+            
+            $.ajax({
+                url : "http://msauniversity.azurewebsites.net/api/Students/" + studentid,
+                type: "PUT",
+                data : student,
+                success: function(data, textStatus, jqXHR)
+                {
                     callback();
                 }
-            }
-
-            xhttp.open("PUT", "http://msauniversity.azurewebsites.net/api/Students/" + studentid, true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-
-            xhttp.send(JSON.stringify(student));
+             });
         },
 
         deleteStudent: function (studentid, callback) {
             
-            var xhttp = new XMLHttpRequest();
-
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
+            $.ajax({ 
+                type: "DELETE",
+                dataType: "json",
+                url: "http://msauniversity.azurewebsites.net/api/Students/" + studentid,
+                success: function(data){        
                     callback();
                 }
-            }
-
-            xhttp.open("DELETE", "http://msauniversity.azurewebsites.net/api/Students/" + studentid, true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-
-            xhttp.send();
+             });
         }
     };
 
